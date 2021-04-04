38dc3fb0497bff6f4bdb6d511e92afc773cf0708
Wellington Espindula <wellington.m.espindula@gmail.com>
2021-04-04 12:49:01 -0300

    Added other project files

---
+++ binary-coe.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# coding: UTF-8
+
+memory_file = "mem.bin"
+output_file = "memory.coe"
+output_file2 = "mem_clean.bin"
+
+file = open(memory_file, "rb")
+coe = open(output_file, "w")
+binf = open(output_file2, "wb")
+
+# ignore first 2 bytes (header)
+byte = file.read(4)
+byte = file.read(1)
+file.read(1)
+
+count = 0
+buffer = "MEMORY_INITIALIZATION_RADIX=16;\nMEMORY_INITIALIZATION_VECTOR=" 
+
+#print(byte.hex())
+
+while count < 255:
+    buffer += "{}, ".format(byte.hex())
+#    print("{}, ".format(byte.hex()))
+    binf.write(byte)
+    byte = file.read(1)
+    file.read(1)
+    count += 1
+
+buffer = buffer[:-2]
+buffer += ";"
+
+#print(buffer)
+coe.write(buffer)
+
+file.close()
+coe.close()
+binf.close()
